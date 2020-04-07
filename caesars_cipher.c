33169da00a7daa09a842d68e8a7edf2e4ec33ad2
tpragik <tpragik@gmail.com>
2020-04-07 09:12:40 +0545

    edx//cs50//caeser's_cipher

--- caesars_cipher.c
+++ caesars_cipher.c
@@ -2,20 +2,17 @@
 #include<cs50.h>
 #include<ctype.h>
 #include<stdlib.h>
-
+#include<math.h>
+#include<string.h>
 int key=0;
-
-int process_key(int argc,string argv[]);
 
 int main(int argc,string argv[])
 {
-    process_key(argc,argv);
-    return 0;
-}
 
-int process_key(int argc,string argv[])
-{
+    // key processing
     string k=NULL;
+
+    //checking for more than 2 arguments
     if(argc>2)
     {
         printf("Usage: ./caesar key\n");
@@ -29,6 +26,8 @@ int process_key(int argc,string argv[])
     else
         k=argv[1];
 
+    //checking if the key is alphanumeric and decimal
+
     for(int i=0;k[i]!='\0';i++)
     {
         if(isalpha(k[i]))
@@ -39,14 +38,52 @@ int process_key(int argc,string argv[])
         else if(k[i]=='.')
         {
             printf("Usage: ./ceasar key\n");
-            return 1;
+           return 1;
         }
     }
     key=atoi(k);
+
+    //checking if the key is -ve integer
     if(key<0)
         {
             printf("Usage: ./caesar key\n");
             return 1;
         }
-    return key;
+
+    //enciphering the recieved string
+
+    string input=get_string("plaintext:\n");
+    for(int i=0;i<strlen(input);i++)
+    {
+        //checking if the string is an alphabet
+        if(isalpha(input[i]))
+        {
+          //checking if the string is uppercase
+          if(isupper(input[i]))
+          {
+                //converting string's ith position to integer and adding key value
+                input[i]=((unsigned int)input[i]+key);
+                //checking if the given value exceeds 90 i.e. Z's ASCII value
+                while((unsigned int)input[i]>=90)
+                {
+                    //if it does subtracting 26 untill the condition is false
+                    input[i]=(unsigned int)input[i]-26;
+                }
+          }
+          else
+          {
+               //similar as to uppercase letters
+                input[i]=((unsigned int)input[i]+key);
+                while((unsigned int)input[i]>=122)
+                {
+                    input[i]=(unsigned int)input[i]-26;
+                }
+          }
+
+        }
+    }
+
+    printf("ciphertext: %s\n",input);
+
+    return 0;
 }

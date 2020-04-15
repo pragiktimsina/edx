ee1121fa8c0b47c681e90d0cf4096217c68b1471
pragiktimsina <tpragik@aol.com>
2020-04-15 15:58:27 +0545

    edx/cs50/readablity

--- readability.c
+++ readability.c
@@ -8,36 +8,55 @@ int main(void)
 {
     string s = get_string("Text: ");
 
-    float letter_count=0;
-    for(int i=0;i<strlen(s);i++)
+//counting the number of letters
+    float letter_count = 0;
+    for (int i = 0; i < strlen(s); i++)
     {
-        if(isalnum(s[i]))
+        if (isalnum(s[i]))
+        {
             letter_count++;
+        }
     }
 
-    float word_count=0;
-    for(int i=0;s[i]!='\0';i++)
-    { if ((int) s[i] == 32)
-            word_count++;
-    }
-
-    float sentence_count=0;
-    for(int i=0;s[i]!='\0';i++)
+//counting the number of words
+    float word_count = 0;
+    for (int i = 0; s[i] != '\0'; i++)
     {
-        if(s[i]=='.'|| s[i]=='?'||s[i]=='!')
-            sentence_count++;
+        if ((int) s[i] == 32)
+        {
+            word_count++;
+        }
     }
 
-    float L = letter_count/(word_count+1)*100;
-    float S = sentence_count/(word_count+1)*100;
+//counting the number of sentences
+    float sentence_count = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+        {
+            sentence_count++;
+        }
+    }
 
+//counting the no. of letters per 100 words
+    float L = letter_count / (word_count + 1) * 100;
+//counting the no. of sentences per 100 words
+    float S = sentence_count / (word_count + 1) * 100;
+//computing Coleman-Liau index
     float (index) = 0.0588 * L - 0.296 * S - 15.8;
-
-    if (roundf(index)>=16)
+//printing the index to the user
+    if (roundf(index) >= 16)
+    {
         printf("Grade 16+\n");
-    else if(roundf(index)<1)
+    }
+    else if (roundf(index) < 1)
+    {
         printf("Before Grade 1\n");
+    }
     else
-        printf("Grade %.0f\n",roundf(index));
+    {
+        printf("Grade %.0f\n", roundf(index));
+    }
+
     return 0;
 }
